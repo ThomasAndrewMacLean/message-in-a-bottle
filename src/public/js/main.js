3909325b61c87bc5772d6e4ca0cd765f59cf5be6
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-02-19 22:18:35 +0100

    share api

---
+++ src/public/js/main.js
@@ -0,0 +1,16 @@
+const clickBtn = () => {
+  console.log(navigator);
+  if (navigator.share) {
+    navigator
+      .share({
+        title: 'Web Fundamentals',
+        text: 'Check out Web Fundamentals — it rocks!',
+        url: 'https://developers.google.com/web'
+      })
+      .then(() => console.log('Successful share'))
+      .catch(error => console.log('Error sharing', error));
+  }
+};
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', clickBtn);
